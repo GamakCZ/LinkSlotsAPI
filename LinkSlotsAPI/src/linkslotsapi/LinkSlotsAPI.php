db301552d0e6c91afb873ab84d2010acaa7b523f
GamakCZ <gamak.mcpe@gmail.com>
2018-01-01 12:34:48 +0100

    Update LinkSlotsAPI.php

--- LinkSlotsAPI/src/linkslotsapi/LinkSlotsAPI.php
+++ LinkSlotsAPI/src/linkslotsapi/LinkSlotsAPI.php
@@ -11,4 +11,17 @@ use pocketmine\plugin\PluginBase;
  * @package linkslotsapi
  * @author VixikCZ
  */
-class LinkSlotsAPI extends PluginBase { public function onEnable() {} }
+class LinkSlotsAPI extends PluginBase {
+  
+  public function onEnable() {
+    $this->startTask();
+  }
+  
+  private function startTask() {
+    $this->getServer()->getScheduler()->scheduleRepeatingTask(new class extends \pocketmine\scheduler\Task {
+      public function onRun(int $currentTick) {
+        \pocketmine\Server::getInstance()->getScheduler()->scheduleAsyncTask(new \linkslotsapi\task\RefreshDataTask());
+      }
+    }, 20*10);
+  }
+}
